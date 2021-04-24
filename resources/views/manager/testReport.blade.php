6feae0d89299ce95ad3a94d527ab1e6fe321b8e8
Ignaz Maula <ignazmaula@gmail.com>
2021-04-25 03:39:43 +0800

    fix css

--- resources/views/manager/testReport.blade.php
+++ resources/views/manager/testReport.blade.php
@@ -9,30 +9,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Counter-Covid CTIS - Dashboard</title>
+    <title>Counter-Covid CTIS - Test Report</title>
 
     <!-- Icon -->
     <link rel="icon" href="/images/favicon.webp">
-
-    <!-- Custom fonts for this template-->
-    <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <link href="/css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="/css/sb-admin-custom.css" rel="stylesheet">
-    
-    <link rel="stylesheet" href="{{ ltrim(public_path('vendor/datatables/dataTables.bootstrap4.min.css'), '/') }}" />
-    <link rel="stylesheet" href="{{ ltrim(public_path('css/sb-admin-2.min.css'), '/') }}" />
-    <link rel="stylesheet" href="{{ ltrim(public_path('css/sb-admin-custom.css'), '/') }}" />
-
-    <link rel="stylesheet" href="{{ public_path('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ public_path('css/sb-admin-2.min.css') }}">
-    <link rel="stylesheet" href="{{ public_path('css/sb-admin-custom.css') }}">
-
     <style>
         body {
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
